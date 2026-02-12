3fc8f8b6cd5fb154d3fd157ec0ab6fbbc63f4323
vedprakas3 <vedprakasharya9973@gmail.com>
2026-02-12 13:56:31 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,31 @@
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav style={navStyle}>
+      <div style={logo}>🔥 Snapforest</div>
+      <div style={menu}>
+        <span>Shop</span>
+        <span>Categories</span>
+        <span>Cart</span>
+        <span>Login</span>
+      </div>
+    </nav>
+  );
 }
+
+const navStyle = {
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "20px 40px",
+  background: "white",
+  boxShadow: "0 2px 10px rgba(0,0,0,0.05)",
+};
+
+const logo = {
+  fontWeight: "bold",
+  fontSize: "20px",
+};
+
+const menu = {
+  display: "flex",
+  gap: "20px",
+};
