072c5442ce28b3bf62d2035039e44076e265104a
vedprakas3 <vedprakasharya9973@gmail.com>
2026-02-12 14:39:30 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,20 @@
-import Hero from "../components/Hero"
-import ProductCard from "../components/ProductCard"
-
-export default function Home() {
-  return (
-    <>
-      <Hero />
-      <section className="products">
-        <h2>Featured Products</h2>
-        <div className="grid">
-          <ProductCard title="Wireless Earbuds" price="1499" />
-          <ProductCard title="Urban Sneakers" price="2499" />
-          <ProductCard title="Premium Hoodie" price="1999" />
-        </div>
-      </section>
-    </>
-  )
-}
+const demoProducts = [
+  {
+    id: 1,
+    name: "Wireless Earbuds",
+    price: 1499,
+    image: "https://images.unsplash.com/photo-1585386959984-a41552262b8c"
+  },
+  {
+    id: 2,
+    name: "Urban Sneakers",
+    price: 2499,
+    image: "https://images.unsplash.com/photo-1600185365483-26d7a4cc7519"
+  },
+  {
+    id: 3,
+    name: "Premium Hoodie",
+    price: 1999,
+    image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf"
+  }
+]
