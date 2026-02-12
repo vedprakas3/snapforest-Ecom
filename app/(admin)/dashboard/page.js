03a4c5bda795bcac5ac05efc26a9bdc96ccbe20c
vedprakas3 <vedprakasharya9973@gmail.com>
2026-02-12 13:29:49 +0530

    Delete app directory

--- app/(admin)/dashboard/page.js
+++
@@ -1,7 +0,0 @@
-export default function AdminDashboard() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Admin Dashboard</h1>
-    </div>
-  );
-}
--- app/(store)/page.js
+++
@@ -1,8 +0,0 @@
-export default function StoreHome() {
-  return (
-    <main style={{ padding: "40px" }}>
-      <h1>Snapforest</h1>
-      <p>Premium Ecommerce Platform</p>
-    </main>
-  );
-}
