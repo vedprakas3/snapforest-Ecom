eb978fb688b42f52f188d7fce1129ca135b48743
vedprakas3 <vedprakasharya9973@gmail.com>
2026-02-12 14:02:30 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer style={footer}>
+      <p>© 2026 Snapforest. All rights reserved.</p>
+    </footer>
+  );
+}
+
+const footer = {
+  padding: "30px",
+  textAlign: "center",
+  background: "#111",
+  color: "white",
+  marginTop: "50px",
+};
