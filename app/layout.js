73a6e1cdbfbb257bf7d75120534f94715756c05f
vedprakas3 <vedprakasharya9973@gmail.com>
2026-02-12 13:59:34 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
+import "../styles/globals.css";
+
 export const metadata = {
   title: "Snapforest",
-  description: "Premium Ecommerce Store"
+  description: "Premium Ecommerce Platform",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, fontFamily: "Arial" }}>
+      <body>
         {children}
       </body>
     </html>
